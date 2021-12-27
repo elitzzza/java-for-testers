616ccb71208fbc5b861a270aa1d7439a741b8f87
elitzzza <srebkova@gmail.com>
2021-12-27 16:17:20 +0200

    exceptions

--- src/main/java/com/serenitydojo/exceptions/FileLoader.java
+++ src/main/java/com/serenitydojo/exceptions/FileLoader.java
@@ -1,19 +1,49 @@
 package com.serenitydojo.exceptions;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 
 public class FileLoader {
-    public String readHelloWorld() throws IOException {
-        return "";//Files.readString(Paths.get("src/main/resources/hello.txt"));
+    public String readHelloWorld() {
+
+        try {
+           String text =  Files.readString(Paths.get("src/main/resources/hello.txt"));
+            return text;
+        } catch (NoSuchFileException noSuchFile) {
+            return "No Such file";
+        }catch(IOException otherException){
+            return "Some other problem";
+        }
+
+
     }
 
     public Boolean fileContainsText(String filename, String expectedText) {
         String path = "src/main/resources/" + filename;
-        return null;// (Files.readString(Paths.get(path)).contains(expectedText));
+        try {
+            boolean containsText = Files.readString(Paths.get(path)).contains(expectedText);
+            return true;
+        } catch (NoSuchFileException noSuchFile) {
+            return false;
+        }catch(IOException textDoesntMatch){
+            return false;
+        }
+
+
     }
 
-    public Boolean fileHasText(String filename, String expectedText) {
+    public Boolean fileHasText(String filename, String expectedText)throws MissingWelcomeFileException {
         String path = "src/main/resources/" + filename;
-        return null;// (Files.readString(Paths.get(path)).contains(expectedText));
+        try {
+           Boolean hasText =  (Files.readString(Paths.get(path)).contains(expectedText));
+            return true;
+        } catch (MissingWelcomeFileException e) {
+            return false;
+        }catch (IOException e){
+            return false;
+        }
+
     }
 }
